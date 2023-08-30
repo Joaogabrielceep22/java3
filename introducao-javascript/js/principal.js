a5174da35e0bfa61fb2a6d35cd1721a517130243
Joaogabrielceep22 <126880851+Joaogabrielceep22@users.noreply.github.com>
2023-08-30 09:52:41 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,5 +1,14 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textCoontet;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(paciente); // tr
+console.log(tdAltura);// td que tem o peso
+console.log(altura); // obter 100
