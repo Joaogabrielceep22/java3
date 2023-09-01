599cdaaba35afffe3241f39939d02fb2ed05d48d
Joaogabrielceep22 <126880851+Joaogabrielceep22@users.noreply.github.com>
2023-09-01 09:52:31 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -4,11 +4,13 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textCoontet;
+var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-console.log(paciente); // tr
-console.log(tdAltura);// td que tem o peso
-console.log(altura); // obter 100
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
