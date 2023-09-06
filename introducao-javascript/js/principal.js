a13b0584dba4027bc55c8d7b9daa6b32820adba2
Joaogabrielceep22 <126880851+Joaogabrielceep22@users.noreply.github.com>
2023-09-06 09:51:35 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -11,6 +11,53 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if( alturaEhValida && pesoEhValido){
+var imc = peso / (altura * altura);
 tdImc.textContent = imc;
+}
+
+
+var paciente = document.querySelector("#segundo-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if( alturaEhValida && pesoEhValido){
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
+}
